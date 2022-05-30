90e434e2e8c3ded465c073b7db2e8a305097b69f
WCalderbank <wcalderbank99@gmail.com>
2022-05-30 15:05:34 +0100

    rough styling

--- app/src/Components/Television.js
+++ app/src/Components/Television.js
@@ -3,17 +3,19 @@ import "./Television.css";
 
 function Television({ data }) {
   return (
-    <div className="show_info">
-      <h2>{data.name}</h2>
-      <img src={data.image.medium} alt="show" />
-      <div className="genres">
-        <h3>{data.genres[0]}</h3>
-        <h3>{data.genres[1]}</h3>
-        <h3>{data.genres[2]}</h3>
-        <h3>{data.genres[3]}</h3>
+    <>
+      <div className="show_info">
+        <h2>{data.name}</h2>
+        <img src={data.image} alt="show" />
+        <div className="genre-box">
+          <h3>{data.genres[0]}</h3>
+          <h3>{data.genres[1]}</h3>
+          <h3>{data.genres[2]}</h3>
+          <h3>{data.genres[3]}</h3>
+        </div>
+        <h1> STATUS: {data.status}</h1>
       </div>
-      <h1> STATUS: {data.status}</h1>
-    </div>
+    </>
   );
 }
 
