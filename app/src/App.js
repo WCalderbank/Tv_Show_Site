e0d2008c284228711244400bd764958a23be4b02
WCalderbank <wcalderbank99@gmail.com>
2022-06-24 10:16:09 +0100

    Update fresh start

--- app/src/App.js
+++ app/src/App.js
@@ -1,28 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import Television from "./Components/Television";
 
 function App() {
-  const [tvData, setTvData] = useState({});
-  async function dataFetch() {
-    const response = await fetch("https://api.tvmaze.com/shows/500");
-    const data = await response.json();
-    console.log(data);
-    setTvData(data);
-  }
-  useEffect(() => {
-    dataFetch();
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>Tv Encyclopedia</h1>
-      <form>
-        <input type="text" placeholder="Search Here" />
-      </form>
-      <Television data={tvData} />
-    </div>
-  );
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
--- app/src/Components/Television.js
+++ app/src/Components/Television.js
@@ -1,22 +1,8 @@
 import React from "react";
 import "./Television.css";
 
-function Television({ data }) {
-  return (
-    <>
-      <div className="show_info">
-        <h2>{data.name}</h2>
-        <img src={data.image} alt="show" />
-        <div className="genre-box">
-          <h3>{data.genres[0]}</h3>
-          <h3>{data.genres[1]}</h3>
-          <h3>{data.genres[2]}</h3>
-          <h3>{data.genres[3]}</h3>
-        </div>
-        <h1> STATUS: {data.status}</h1>
-      </div>
-    </>
-  );
+function Television() {
+  return <div className="show_info"></div>;
 }
 
 export default Television;
