9c5a372b99dcb3ac810698b2bc62d5ed148e61a3
WCalderbank <wcalderbank99@gmail.com>
2022-08-19 19:29:58 +0100

    Organised Components

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Components/Header";
+import Search from "./Components/Search";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Search />
     </div>
   );
 }
--- app/src/Components/Television.js
+++ app/src/Components/Television.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Television.css";
 
 function Television() {
-  return <div className="show_info"></div>;
+  return (
+    <div className="show_info">
+      <h3>Display Area</h3>
+    </div>
+  );
 }
 
 export default Television;
