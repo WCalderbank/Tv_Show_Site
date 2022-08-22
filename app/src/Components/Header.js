20187841e6f4e2b4ea9a70c033366fd021a54778
WCalderbank <wcalderbank99@gmail.com>
2022-08-22 16:51:53 +0100

    minor styling

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 function Header() {
   return (
     <div>
-      <h1>Welcome To My Tv-Site!</h1>
+      <h1 className="main-header">Welcome To My Tv-Site!</h1>
     </div>
   );
 }
